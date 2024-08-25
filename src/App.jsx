fb15f5dbb5f216c37d188d7ac062cc07123fb925
Ruby Da Cherry <yaghoubiarshia1381@gmail.com>
2024-08-25 13:33:06 +0330

    starter files(added styled-components library)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles.js";
+import Button from "./ui/Button.jsx";
+import Heading from "./ui/Heading.jsx";
+import Input from "./ui/Input.jsx";
+import Row from "./ui/Row.jsx";
 
 function App() {
-    return (
-        <h1>Hiii</h1>
-    )
+  return (
+    <>
+      <GlobalStyles />
+      <Row type={"vertical"}>
+        <Row type={"horizontal"}>
+          <Heading as={"h1"}>The Wild Oasis</Heading>
+          <div>
+            <Heading as={"h2"}>Check in and out</Heading>
+            <Button variation={"primary"} size={"medium"}>
+              Check in
+            </Button>
+            <Button variation={"secondary"} size={"small"}>
+              Check out
+            </Button>
+          </div>
+        </Row>
+        <Row type={"vertical"}>
+          <Heading as={"h3"}>Forms</Heading>
+          <from>
+            <Input placeholder={"text"} />
+            <Input placeholder={"number"} />
+          </from>
+        </Row>
+      </Row>
+    </>
+  );
 }
 
-export default App
+export default App;
