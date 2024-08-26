a80dbb7a49130b682a502157b43bdc1cba4eb02e
Ruby Da Cherry <yaghoubiarshia1381@gmail.com>
2024-08-26 14:05:54 +0330

    installing supabase and creating tables

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,24 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { useEffect } from "react";
+import { getCabins } from "../services/apiCabins.js";
+import logo from "../ui/Logo.jsx";
 
 function Cabins() {
+  useEffect(() => {
+    getCabins().then((data) => console.log(data));
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        alt={"pic"}
+        src={
+          "https://reooetdkicqhvxriudwv.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
+        }
+      />
     </Row>
   );
 }
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://reooetdkicqhvxriudwv.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlb29ldGRraWNxaHZ4cml1ZHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ2NTc4NDYsImV4cCI6MjA0MDIzMzg0Nn0.SvOJLXMfeS3-cfSk2bvzE7RRMlFolwfgPIOvFvu2C30";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
