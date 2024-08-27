cafc018f9f33665fbf20877c65f85694faeec0ec
Ruby Da Cherry <yaghoubiarshia1381@gmail.com>
2024-08-27 12:47:37 +0330

    installing react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import GlobalStyles from "./styles/GlobalStyles.js";
 import Dashboard from "./pages/Dashboard.jsx";
@@ -10,10 +11,21 @@ import PageNotFound from "./pages/PageNotFound.jsx";
 import Settings from "./pages/Settings.jsx";
 import Users from "./pages/Users.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // staleTime: 60 * 1000,
+      staleTime: 0,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -30,7 +42,7 @@ function App() {
           <Route path={"*"} element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,25 +1,19 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { useEffect } from "react";
-import { getCabins } from "../services/apiCabins.js";
-import logo from "../ui/Logo.jsx";
+import CabinTable from "../features/cabins/CabinTable.jsx";
 
 function Cabins() {
-  useEffect(() => {
-    getCabins().then((data) => console.log(data));
-  }, []);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img
-        alt={"pic"}
-        src={
-          "https://reooetdkicqhvxriudwv.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
-        }
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>TEST</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
